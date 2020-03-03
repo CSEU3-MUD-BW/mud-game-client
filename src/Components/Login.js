77e1316b77b71470aa2f4986f3d3d6f7b420a419
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-03-03 09:32:42 +0100

    login component completed

---
+++ src/Components/Login.js
@@ -0,0 +1,71 @@
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { login } from '../redux/actions/actionCreators';
+import { Link } from 'react-router-dom';
+
+const Login = props => {
+
+    // const email = useRef();
+    const username = useRef();
+    const password = useRef();
+
+
+    const handleSubmit = () => {
+
+        const userData = {
+            username: username.current.value,
+            // email: email.current.value,
+            password: password.current.value,
+        }
+
+        if (userData.username && userData.password) {
+            props.login(userData)
+            // props.history.push('/home');
+
+        }
+        
+    }
+
+    if (props.token){
+        props.history.push('/home')
+    }
+    // console.log(props)
+    return (
+        <div>
+            Welcome to the login page
+
+            <div>
+                <input type="text"  placeholder="UserName" required ref={username} />
+            </div>
+
+            {/* <div>
+                <input type="email" placeholder="Email" required />
+            </div> */}
+
+            <div>
+                <input type="password" placeholder="Password" required  ref={password} />
+            </div>
+
+            <button onClick={handleSubmit}> Login </button>
+
+            <p>
+            {" "}
+                Dont have an account yet? &nbsp; <Link to="/signup"> Sign up here </Link>{" "}
+            </p>
+
+        </div>
+    )
+}
+
+const mapStateToProps = store => {
+    return {
+        loading: store.authentication.loading,
+        token: store.authentication.token,
+        error: store.authentication.error 
+    }
+}
+
+const mapDispatchToProps = {
+    login
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
