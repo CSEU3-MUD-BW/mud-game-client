74ece52f079ba8316722eed9af73004e6345f779
CarnunMP <carnun@hotmail.co.uk>
2020-03-04 22:10:41 +0000

    Added action creator for getting rooms.

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -2,7 +2,7 @@ import * as types from './actionTypes';
 import axios from 'axios';
 
 
-export const apiURL = ' https://lambda-mud-test.herokuapp.com/api/';
+export const apiURL = ' https://lambda-mud-test.herokuapp.com/api';
 
 
 
@@ -10,7 +10,7 @@ export const signup = userData => dispatch => {
     dispatch({ type: types.SIGNUP_START });
     console.log('im thereeeeeeeees')
     axios
-    .post(`${apiURL}registration/`, userData)
+    .post(`${apiURL}/registration/`, userData)
       .then(res => {
         localStorage.setItem('token', res.data.key)
         dispatch({ type: types.SIGNUP_SUCCESS, payload: res.data.key });
@@ -25,7 +25,7 @@ export const signup = userData => dispatch => {
 export const login = userData => dispatch => {
     dispatch({ type: types.LOGIN_START })
     return axios
-    .post(`${apiURL}login/`, userData)
+    .post(`${apiURL}/login/`, userData)
       .then(res => {
         localStorage.setItem('token', res.data.key)
         dispatch({ type: types.LOGIN_SUCCESS, payload: res.data.key});
@@ -35,3 +35,18 @@ export const login = userData => dispatch => {
         dispatch({ type: types.LOGIN_FAILURE });
       });
   }
+
+
+  // Map action creators
+  export const getRooms = () => dispatch => {
+    dispatch({ type: types.GET_ROOMS });
+
+    return axios.get(`${apiURL}/adv/rooms`)
+      .then(res => {
+        dispatch({ type: types.GET_ROOMS_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch({ type: types.GET_ROOMS_FAILURE });
+      });
+  } 
